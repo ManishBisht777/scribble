0eef0e83bbb741a25573ceb51f4d725e3e0fd9c9
ManishBisht7 <manishbisht9711@gmail.com>
2023-08-31 17:27:24 +0530

    Feat: initial app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button variant="outline" size="lg">
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
