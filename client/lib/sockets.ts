1d809cf6af4996c1a0fc7e49f1da22f08d67d555
ManishBisht7 <manishbisht9711@gmail.com>
2023-09-20 12:53:54 +0530

    fix: port error

--- client/lib/sockets.ts
+++ client/lib/sockets.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SERVER =
   process.env.NODE_ENV === "production"
-    ? "https://draw-with-me-socket-server.herokuapp.com/"
-    : "http://localhost:3001";
+    ? "https://scribble-production-f06d.up.railway.app/"
+    : "https://scribble-production-f06d.up.railway.app/";
 
 export const socket = io(SERVER, { transports: ["websocket"] });
